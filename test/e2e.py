69e93acbf674b399a741910d02366fe684fb504f
peleS <pele220@gmail.com>
2025-03-11 14:11:00 +0200

    Update e2e.py

--- test/e2e.py
+++ test/e2e.py
@@ -1,24 +1,28 @@
-import time
 import sys
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def test_scores_service(url):
     """Test that the score is between 1 and 1000."""
     options = Options()
-    options.add_argument("--headless")  # Run in headless mode for automation
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    options.binary_location = "/usr/bin/google-chrome"
+
     service = Service("/usr/local/bin/chromedriver")
+    service.start()
     driver = webdriver.Chrome(service=service, options=options)
 
     try:
         driver.get(url)
-        time.sleep(2)
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.TAG_NAME, "h1"))
+        )
         score_text = driver.find_element(By.TAG_NAME, "h1").text.replace("The score is: ", "").strip()
         score = int(score_text)
 
@@ -28,13 +32,12 @@ def test_scores_service(url):
         return False
     finally:
         driver.quit()
-
+        service.stop()
 
 def main():
     url = "http://localhost:8777"
     test_result = test_scores_service(url)
     sys.exit(0 if test_result else -1)
-
 
 if __name__ == "__main__":
     main()
